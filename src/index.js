2ad0ec306fbe2cc29df0429e9377a4de4158c1af
fullcrimp <fullcrimp@protonmail.com>
2017-10-29 18:45:19 +0300

    add brute force solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let longest = 0,
+    sequenceCount = 0,
+    starts = {},
+    ends = {},
+    lengths = {}
+
+  for (let i = 0; i < array.length; i++) {
+    let value = array[i]
+    let prevId = ends[value - 1],
+        nextId = starts[value + 1]
+
+    // a piece is in-between two sequences
+    if (prevId && nextId) {
+
+      delete starts[value + 1]
+
+      lengths[prevId] += lengths[nextId] + 1
+      longest = (longest < lengths[prevId]) ? lengths[prevId] : longest
+      continue
+    }
+
+    // the piece is positioned right before a sequence
+    if (nextId) {
+
+      // swop starts
+      delete starts[value + 1]
+      starts[value] = nextId
+
+      lengths[nextId] += 1
+      longest = (longest < lengths[nextId]) ? lengths[nextId] : longest
+      continue
+    }
+
+    // the piece is positioned right after a sequence
+    if (prevId) {
+
+      // swop ends
+      delete ends[value - 1]
+      ends[value] = prevId
+
+      lengths[prevId] += 1
+      longest = (longest < lengths[prevId]) ? lengths[prevId] : longest
+      continue
+    }
+
+    // add a new sequence
+    id = ++sequenceCount
+    starts[value] = id
+    ends[value] = id
+    lengths[id] = 1
+  }
+  return longest
 }
